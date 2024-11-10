ba61c588c4fe52bb14f00ed7c7f0cfdd7d7aea44
p23av <p23av@yandex.ru>
2024-11-10 21:20:49 +0300

    lesson 1

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Алексей'
+print(name)
+age = 34
+print(age)
+age = age + 5
+print(age)
+is_student = True
+print(is_student)
---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+hw_count = 12
+hours = 1.5
+course_name = 'Python'
+time_on_hw = hours / hw_count
+print('Курс:', course_name, ', всего задач:', hw_count, ', затрачено часов: ', hours, ', среднее время выполнения', time_on_hw, 'часа.')
