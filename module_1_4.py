690a306f2ce9d698c2e6d1c64b2afc8bc58848d6
p23av <p23av@yandex.ru>
2024-11-14 09:49:06 +0300

    lesson2

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,7 @@
-hw_count = 12
-hours = 1.5
-course_name = 'Python'
-time_on_hw = hours / hw_count
-print('Курс:', course_name, ', всего задач:', hw_count, ', затрачено часов: ', hours, ', среднее время выполнения', time_on_hw, 'часа.')
+my_string = input()
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
