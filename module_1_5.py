9bf5faa95fd051d09b3cc02e00b95700de6f7bce
Aleksey Pasiukov <pasiukov@bolid.ru>
2024-11-16 12:59:28 +0300

    Неизменяемые и изменяемые объекты. Кортежи

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = (1, '2', False, [0,2])
 print(immutable_var)
-immutable_var[0] = True # Ошибка - кортеж неизменяем
+# immutable_var[0] = True # Ошибка - кортеж неизменяем
 mutable_list = [1,2,3]
 mutable_list[0] = False
 mutable_list[1] = True
