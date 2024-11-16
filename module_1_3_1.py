2114d6f6f6e62f6f71f12993d0942162bc4e426e
Aleksey Pasiukov <pasiukov@bolid.ru>
2024-11-16 12:51:17 +0300

    Организация программ и методы строк

---
+++ module_1_3_1.py
@@ -0,0 +1,5 @@
+count_hw = 12
+count_hours = 1.5
+course_name = 'Python'
+time_per_lesson = count_hours / count_hw
+print('Курс:', course_name, ', всего задач:', count_hw, ', затрачено часов: ', count_hours, ', среднее время выполнения', time_per_lesson, 'часа.')
