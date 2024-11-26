64346c889917516006db933b12a9c9afeebf15a6
Aleksey Pasiukov <pasiukov@bolid.ru>
2024-11-26 21:49:37 +0300

    Различие атрибутов класса и экземпляра

--- module_5/module_5_4.py
+++ module_5/module_5_4.py
@@ -1,7 +1,8 @@
-from module_3.module_3_3 import values_dict
-
-
 class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
@@ -39,8 +40,8 @@ class House:
         if isinstance(value, int):
             self.number_of_floors += value
             return self
-
-
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
     def go_to(self, new_floor ):
         if new_floor > self.number_of_floors or new_floor < 1:
             print('Такого этажа не существует')
@@ -48,26 +49,16 @@ class House:
             for i in range(1, new_floor + 1):
                 print(i)
 
+
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = 10 + h2 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(House.houses_history)
